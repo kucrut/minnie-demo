145201a350ab3a0efe522b96db6142dad95259c0
Dzikri Aziz <kvcrvt@gmail.com>
2016-09-14 08:39:06 +0800

    Add base PHPunit files

---
+++ .tests/wp-casperjs-tests-config.php
@@ -0,0 +1,10 @@
+<?php
+
+define( 'HM_ENV', 'circleci' );
+
+define( 'DB_NAME', 'wordpress_test' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', 'localhost' );
+
+define( 'DISABLE_WP_CRON', true );
